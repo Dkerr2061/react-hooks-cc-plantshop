ff1d0ab2f1d010269759fa08e24398f2e0a81522
David Kerr <kerrky@gmail.com>
2024-02-28 13:39:37 -0500

    Complete Challenge

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant, onDeletePlant, onEditPlantPrice}) {
+  const [ inStock, setInstock ] = useState(true)
+  const [ updatePrice, setUpdatePrice] = useState('')
+  
+  function isInStock() {
+    setInstock(inStock => !inStock)
+  }
+
+  function handleDeleteButton() {
+    onDeletePlant(plant.id)
+  }
+
+  function handlePriceChange(event) {
+    setUpdatePrice(event.target.value)
+  }
+
+  function handleNewPriceSubmit(event) {
+    event.preventDefault()
+    const parsedPrice = parseFloat(updatePrice)
+    onEditPlantPrice({price: parsedPrice}, plant.id)
+  }
+  
+ 
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+        <form onSubmit={handleNewPriceSubmit}>
+          <input className="update-price" type="number" step="0.01" placeholder="Update Price" onChange={handlePriceChange}/>
+          <button>Update Price</button>
+        </form>
+      {inStock ? (
+        <button className="primary" onClick={isInStock}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={isInStock}>Out of Stock</button>
       )}
+      <button onClick={handleDeleteButton}>Delete Plant</button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,93 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [ plants, setPlants] = useState([])
+  const [ searchPlants, setSearchPlants ] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(res => res.json())
+      .then(plantData => setPlants(plantData))
+  }, [])
+
+  function onSearch(event) {
+    setSearchPlants(event.target.value)
+  }
+
+  const filteredPlants = plants.filter(plant => {
+    if(searchPlants === '') {
+      return true
+    } else {
+      return plant.name.toLowerCase().includes(searchPlants.toLowerCase())
+    }
+  })
+
+  function addPlant(newPlant) {
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/JSON'
+      },
+      body: JSON.stringify({
+        "name": newPlant.name,
+        "image": newPlant.image,
+        "price": newPlant.price
+      })
+    })
+      .then(res => res.json())
+      .then(newPlantData => setPlants([...plants, newPlantData]))
+  }
+
+  function editPlantPrice(updatedPrice, id) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "Application/JSON"
+      },
+      body: JSON.stringify(updatedPrice)
+    })
+      .then(res => res.json())
+      .then(newUpdatedPriceData => setPlants(plants => plants.map(plant => {
+        if(plant.id === newUpdatedPriceData.id) {
+          return newUpdatedPriceData
+        } else {
+          return plant
+        }
+      })))
+  }
+
+  function deletePlant(id) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE"
+    })
+      .then(res => {
+        if(res.ok) {
+          return setPlants(plants => plants.filter(plant => {
+            return plant.id !== id
+          }))
+        } else {
+          return alert('Unable to delete plant, try again later.')
+        }
+      })
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm 
+        addPlant={addPlant}
+      />
+      <Search 
+        searchPlants={searchPlants} 
+        onSearch={onSearch}
+      />
+      <PlantList 
+        plants={filteredPlants} 
+        onDeletePlant={deletePlant} 
+        onEditPlantPrice={editPlantPrice}
+      />
     </main>
   );
 }
